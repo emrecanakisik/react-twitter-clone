338fc370a3fe508539b1e5ffc226bb6935b5fbc0
emrecanakisik <emrecanakisik2004@gmail.com>
2023-02-26 06:23:19 +0300

    'PostTab' component updated.

--- src/components/PostTab.tsx
+++ src/components/PostTab.tsx
@@ -9,7 +9,7 @@ const PostTab: FC<PostTabTitle> = ({ title, id }) => {
   return (
     <button
       id={id}
-      onClick={(event) => isClicked(event.target, id)}
+      onClick={() => isClicked(id)}
       className="bg-transparent hover:bg-gray-200 duration-300 w-1/2 flex flex-col items-center px-3.5 relative"
     >
       <span className="text-base duration-300 font-semibold text-gray-600 relative h-12 flex flex-col items-center pt-3">
@@ -25,20 +25,16 @@ export default PostTab;
 let postTabElementSpan = document.querySelector("empty");
 let postTabElementSpanDiv = document.querySelector("empty");
 let oldElement = document.querySelector("#root");
-function isClicked(element: any, id: string) {
-  postTabElementSpan?.classList.add("font-semibold");
-  postTabElementSpan?.classList.add("text-gray-600");
-  postTabElementSpan?.classList.remove("font-bold");
-  postTabElementSpan?.classList.remove("text-gray-700");
+function isClicked(id: string) {
+  postTabElementSpan?.classList.add("font-semibold", "text-gray-600");
+  postTabElementSpan?.classList.remove("font-bold", "text-gray-700");
 
   postTabElementSpanDiv?.classList.add("w-0");
   postTabElementSpanDiv?.classList.remove("w-full");
 
   postTabElementSpan = document.querySelector(`#${id} span`);
-  postTabElementSpan?.classList.remove("font-semibold");
-  postTabElementSpan?.classList.remove("text-gray-600");
-  postTabElementSpan?.classList.add("font-bold");
-  postTabElementSpan?.classList.add("text-gray-700");
+  postTabElementSpan?.classList.remove("font-semibold", "text-gray-600");
+  postTabElementSpan?.classList.add("font-bold", "text-gray-700");
 
   postTabElementSpanDiv = document.querySelector(`#${id} span div`);
   postTabElementSpanDiv?.classList.remove("w-0");
